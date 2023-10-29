624c16933fc38b70a3fb23a4669778734fa1f3f5
Anambibii <F2020266163@umt.edu.pk>
2023-10-29 23:01:07 +0500

    calculator is ready

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Assignment-1
 Assignment 01  Python Programming Open Source Assignment
+This assignemnt is realted to some python code
---
+++ calculator.py
@@ -0,0 +1,14 @@
+input_var1 = int(input("Enter First number : "))
+input_var2 = int(input("Enter Second number : "))
+
+add = input_var1 + input_var2
+sub = input_var1 - input_var2
+multiply = input_var1 *input_var2
+divide = input_var1 / input_var2
+mode = input_var1 % input_var2
+print(" ")
+print("Addition of two numbers: ", add)
+print("Subtraction of two numbers: ", sub)
+print("Multiplication of two numbers: ", multiply)
+print("Division of two numbers: ", divide)
+print("Mode of two numbers: ", mode)
